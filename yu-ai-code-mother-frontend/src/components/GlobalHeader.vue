5daf39e42394a22bf79b58e4855b74f9e5e6514d
cui <2769601102@qq.com>
2025-11-27 11:17:00 +0800

    # 三期：用户模块，前端实现。 --- - 新建路由、获取当前用户、用户注册页面、用户注销、用户管理、权限控制

--- yu-ai-code-mother-frontend/src/components/GlobalHeader.vue
+++ yu-ai-code-mother-frontend/src/components/GlobalHeader.vue
@@ -17,7 +17,25 @@
       <!-- 右侧：用户操作区域 -->
       <a-col>
         <div class="user-login-status">
-          <a-button type="primary">登录</a-button>
+          <div v-if="loginUserStore.loginUser.id">
+            <a-dropdown>
+              <a-space>
+                <a-avatar :src="loginUserStore.loginUser.userAvatar" />
+                {{ loginUserStore.loginUser.userName ?? '无名' }}
+              </a-space>
+              <template #overlay>
+                <a-menu>
+                  <a-menu-item @click="doLogout">
+                    <LogoutOutlined />
+                    退出登录
+                  </a-menu-item>
+                </a-menu>
+              </template>
+            </a-dropdown>
+          </div>
+          <div v-else>
+            <a-button type="primary" href="/user/login">登录</a-button>
+          </div>
         </div>
       </a-col>
     </a-row>
@@ -25,36 +43,60 @@
 </template>
 
 <script setup lang="ts">
-  import { h, ref } from 'vue'
+  import { computed, h, ref } from 'vue'
   import { useRouter } from 'vue-router'
   import type { MenuProps } from 'ant-design-vue'
+  import { message } from 'ant-design-vue'
+  import { useLoginUserStore } from '@/stores/loginUser.ts'
+  import { LogoutOutlined } from '@ant-design/icons-vue'
+  import { userLogout } from '@/api/userController.ts'
+
+  //获取用户登录状态
+  const loginUserStore = useLoginUserStore()
 
   const router = useRouter()
   // 当前选中菜单
   const selectedKeys = ref < string[] > (['/'])
   // 监听路由变化，更新当前选中菜单
   router.afterEach((to, from, next) => {
-    selectedKeys.value =  [to.path]
+    selectedKeys.value = [to.path]
   })
 
   // 菜单配置项
-  const menuItems = ref([
+  const originItems = [
     {
       key: '/',
-      label: '首页',
-      title: '首页',
+      label: '主页',
+      title: '主页',
     },
     {
-      key: '/about',
-      label: '关于',
-      title: '关于我们',
+      key: '/admin/userManage',
+      label: '用户管理',
+      title: '用户管理',
     },
     {
       key: 'others',
       label: h('a', { href: 'https://www.codefather.cn', target: '_blank' }, '编程导航'),
       title: '编程导航',
     },
-  ])
+  ]
+
+  // 过滤菜单项
+  const filterMenus = (menus = [] as MenuProps['items']) => {
+    return menus?.filter((menu) => {
+      const menuKey = menu?.key as string
+      if (menuKey?.startsWith('/admin')) {
+        const loginUser = loginUserStore.loginUser
+        if (!loginUser || loginUser.userRole !== 'admin') {
+          return false
+        }
+      }
+      return true
+    })
+  }
+
+  // 展示在菜单的路由数组
+  const menuItems = computed < MenuProps['items'] > (() => filterMenus(originItems))
 
   // 处理菜单点击
   const handleMenuClick: MenuProps['onClick'] = (e) => {
@@ -65,6 +107,21 @@
       router.push(key)
     }
   }
+
+  // 用户注销
+  const doLogout = async () => {
+    const res = await userLogout()
+    if (res.data.code === 0) {
+      loginUserStore.setLoginUser({
+        userName: '未登录',
+      })
+      message.success('退出登录成功')
+      await router.push('/user/login')
+    } else {
+      message.error('退出登录失败，' + res.data.message)
+    }
+  }
+
 </script>
 
 <style scoped>
